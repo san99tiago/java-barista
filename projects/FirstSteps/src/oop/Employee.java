626869fed5d03397602a4b9c1aedd027e6f207ca
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-01-27 17:02:42 -0500

    Updated FirstSteps employee clases with static method for ids and constructor overload advantages

--- projects/FirstSteps/src/oop/Employee.java
+++ projects/FirstSteps/src/oop/Employee.java
@@ -9,27 +9,60 @@ public class Employee {
 
 	// Create attributes (better as private) for the employee object
 	private String name;
+	private String area;
 	private double salary;
 	private Date jobEndsOn;
 
-	// Constructor method must have the same name as class (ex: "Employee")
-	public Employee(String name, double salary, int year, int month, int day) {
+	// Important: a great example of using "static" method and keeping OOP rules...
+	// ... because "id" is unique for each object, but idNext must be static...
+	// ... to be able to access it for future object references
+	private int id;
+	private static int idNext = 1;
+
+	// Constructor 1 (for overloading and "extending" arguments handling)
+	public Employee(String name, String area, double salary, int year, int month, int day) {
+
 		this.name = name;
+		this.area = area;
 		this.salary = salary;
+
 		// Remark: for GregorianCalendar objects, months start on zero
 		GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
 		jobEndsOn = calendar.getTime();
+
+		// Good way of having unique "id"s without breaking OOP rules...
+		// ...(with static idNext and keeping private attributes for both)
+		id = idNext;
+		idNext++;
 	}
-	
+
+	// Constructor 2 (for overloading and "extending" arguments handling)
+	public Employee(String name, String area, double salary) {
+
+		// Extend main constructor with default date (after that, we change the date)
+		this(name, area, salary, 0, 0, 0);
+
+		// Set default job's end date one year after creation (overwrite jobEndsOn)
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.add(GregorianCalendar.YEAR, 1);
+		jobEndsOn = calendar.getTime();
+
+	}
+
 	// Getter for all information of employee
 	public String getEmployeeInfo() {
-		return "* name: " + name + "\n" + "* Salary: " +  String.valueOf(salary) + "\n"
-				+ "* Job Ends On:  " + String.valueOf(jobEndsOn) + "\n";	
+		return "* name: " + name + "\n* ID: " + String.valueOf(id) + "\n* Area: " + String.valueOf(area)
+				+ "\n* Salary: " + String.valueOf(salary) + "\n* Job Ends On: " + String.valueOf(jobEndsOn) + "\n";
 	}
-	
+
 	// Setter for applying raise to employee's salary
 	public void increseSalary(double percentage) {
-		salary = salary * (1 + percentage/100);
+		salary = salary * (1 + percentage / 100);
+	}
+
+	// Setter for changing area of employee
+	public void changeArea(String new_area) {
+		area = new_area;
 	}
 
 }
--- projects/FirstSteps/src/oop/UseEmployee.java
+++ projects/FirstSteps/src/oop/UseEmployee.java
@@ -14,27 +14,35 @@ public class UseEmployee {
 
 	public static void testEmployees1() {
 		// Create instances of the object Employee
-		Employee employee_1 = new Employee("Santiago Garcia", 100, 2021, 07, 28);
-		Employee employee_2 = new Employee("Santiago Mendez", 150, 2022, 12, 15);
-		Employee employee_3 = new Employee("Leonardo Davinci", 1200, 2025, 01, 01);
+		Employee employee_1 = new Employee("Santiago Garcia", "Engineering", 100, 2021, 07, 28);
+		Employee employee_2 = new Employee("Santiago Mendez", "Testing", 150, 2024, 12, 15);
+		Employee employee_3 = new Employee("Leonardo Davinci", "Psychology", 500);
 
 		System.out.println("\n----- INITIAL STATE OF EMPLOYEES -----");
 		System.out.println(employee_1.getEmployeeInfo());
 		System.out.println(employee_2.getEmployeeInfo());
 		System.out.println(employee_3.getEmployeeInfo());
 
+		// Generate salary increases
 		employee_1.increseSalary(5);
 		employee_2.increseSalary(10);
 		employee_3.increseSalary(21);
+
+		// Apply area changes
+		employee_1.changeArea("Tech Leader");
 
 		System.out.println("\n----- SECOND STATE OF EMPLOYEES -----");
 		System.out.println(employee_1.getEmployeeInfo());
 		System.out.println(employee_2.getEmployeeInfo());
 		System.out.println(employee_3.getEmployeeInfo());
 
+		// Generate salary increases
 		employee_1.increseSalary(8);
 		employee_2.increseSalary(6);
 		employee_3.increseSalary(18);
+
+		// Apply area changes
+		employee_3.changeArea("CEO");
 
 		System.out.println("\n----- FINAL STATE OF EMPLOYEES -----");
 		System.out.println(employee_1.getEmployeeInfo());
@@ -46,32 +54,41 @@ public class UseEmployee {
 		// Create instances of the object Employee in an array
 		Employee[] employees = new Employee[3];
 
-		employees[0] = new Employee("Santiago Garcia", 100, 2021, 07, 28);
-		employees[1] = new Employee("Santiago Mendez", 150, 2022, 12, 15);
-		employees[2] = new Employee("Leonardo Davinci", 1200, 2025, 01, 01);
+		employees[0] = new Employee("Santiago Garcia", "Engineering", 100, 2021, 07, 28);
+		employees[1] = new Employee("Santiago Mendez", "Testing", 150, 2024, 12, 15);
+		employees[2] = new Employee("Leonardo Davinci", "Psychology", 500);
 
 		System.out.println("\n----- INITIAL STATE OF EMPLOYEES -----");
 		for (int i = 0; i < employees.length; i++) {
 			System.out.println(employees[i].getEmployeeInfo());
 		}
 
+		// Generate salary increases
 		employees[0].increseSalary(5);
 		employees[1].increseSalary(10);
 		employees[2].increseSalary(21);
+
+		// Apply area changes
+		employees[0].changeArea("Tech Leader");
 
 		System.out.println("\n----- SECOND STATE OF EMPLOYEES -----");
 		for (int i = 0; i < employees.length; i++) {
 			System.out.println(employees[i].getEmployeeInfo());
 		}
 
+		// Generate salary increases
 		employees[0].increseSalary(8);
 		employees[1].increseSalary(6);
 		employees[2].increseSalary(18);
+
+		// Apply area changes
+		employees[2].changeArea("CEO");
 
 		System.out.println("\n----- FINAL STATE OF EMPLOYEES -----");
 		for (int i = 0; i < employees.length; i++) {
 			System.out.println(employees[i].getEmployeeInfo());
 		}
+
 	}
 
 }
