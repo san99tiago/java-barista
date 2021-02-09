a9d45c7c8da6667ad8f45ac6a5b33d64d5ffaff7
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-02-08 19:54:39 -0500

    Updated SpringProject4 with PUT and DELETE functionalities for REST API

--- projects/SpringProject4_POC/src/com/dao/ClientDAOInterface.java
+++ projects/SpringProject4_POC/src/com/dao/ClientDAOInterface.java
@@ -4,6 +4,9 @@ import com.orm.ClientORM;
 
 public interface ClientDAOInterface {
 
+	public boolean setNewClient(ClientORM newClientORM);
 	public String getClients();
-	boolean setNewClient(ClientORM newClientORM);
+	public String getClientByIdParams(String idType, String idValue);
+	public String updateClientByIdParams(String idType, String idValue, ClientORM updatedClientORM);
+	public String deleteClientByIdParams(String idType, String idValue);
 }
