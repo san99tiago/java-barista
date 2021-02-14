263cd293029b51c771af9940529a8bfaa86e5db3
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-02-14 17:39:18 -0500

    Updated SpringProject1 refactored code with Maven dependencies and SpringBoot

---
+++ projects/SpringProject1/src/ioc/SpringProject1.java
@@ -0,0 +1,13 @@
+package ioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject1.class, args);
+	}
+
+}
