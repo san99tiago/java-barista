c46d341936e0ff87bacc87077081a3fc1376badd
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-02-14 15:04:17 -0500

    Updated SpringProject5 with Swagger documentation

--- projects/SpringProject5_Challenge/src/main/java/com/challenge/controller/ClientPhotoController.java
+++ projects/SpringProject5_Challenge/src/main/java/com/challenge/controller/ClientPhotoController.java
@@ -14,8 +14,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.challenge.model.ClientPhotoORM;
 import com.challenge.service.ClientPhotoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @Controller
 @RequestMapping(value = "/clientPhotos")
+@Api(tags = "clientPhotos")
 public class ClientPhotoController {
 
 	@Autowired
@@ -23,24 +27,28 @@ public class ClientPhotoController {
 
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	@ApiOperation(value = "Get all clientPhotos.")
 	public List<ClientPhotoORM> getClientPhotos() {
 		return clientPhotoService.getClientPhotos();
 	}
-	
+
 	@RequestMapping(value = "/idType/{idType}/idValue/{idValue}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	@ApiOperation(value = "Get clientPhoto by ID parameters.", notes = "Both (idType) and (idValue) are mandatory.")
 	public List<ClientPhotoORM> getClientPhotoByIdParams(@PathVariable String idType, @PathVariable String idValue) {
 		return clientPhotoService.getClientPhotoByIdParams(idType, idValue);
 	}
 
 	@RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	@ApiOperation(value = "Add new clientPhoto.", notes = "If the clientPhto exists, it won't create duplicates.")
 	public ClientPhotoORM addClientPhoto(@RequestBody ClientPhotoORM clientPhotoORM) {
 		return clientPhotoService.addClientPhoto(clientPhotoORM);
 	}
 
 	@RequestMapping(value = "/idType/{idType}/idValue/{idValue}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	@ApiOperation(value = "Update existing clientPhoto by ID parameters.", notes = "If the clientPhoto doesn't exist, it won't create a new one.")
 	public ClientPhotoORM updateClientPhoto(@RequestBody ClientPhotoORM clientPhotoORM, @PathVariable String idType,
 			@PathVariable String idValue) {
 		return clientPhotoService.updateClientPhoto(idType, idValue, clientPhotoORM);
@@ -48,6 +56,7 @@ public class ClientPhotoController {
 
 	@RequestMapping(value = "/idType/{idType}/idValue/{idValue}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	@ApiOperation(value = "Delete existing clientPhoto by ID parameters.", notes = "It will only return a (Done) message, when the clientPhoto existed.")
 	public String deleteClientPhotoByIdParams(@PathVariable String idType, @PathVariable String idValue) {
 		return clientPhotoService.deleteClientPhotoByIdParams(idType, idValue);
 	}
